c0dceeef2fd46a94aae0df1ade5b5d7647f6d417
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-03-23 13:58:13 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./app/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+  	path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   module: {
@@ -11,5 +12,9 @@ module.exports = {
     	{ test: /\.js$/, use: "babel-loader" },
     	{ test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
-  }
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({template: './app/index.html'})
+  ]
 };
