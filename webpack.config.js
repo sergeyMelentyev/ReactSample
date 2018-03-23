42032c47868bbcfc35afd18227ede8bb537ef513
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-03-23 13:12:41 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+    	{ test: /\.js$/, use: "babel-loader" },
+    	{ test: /\.css$/, use: ["style-loader", "css-loader"] }
+    ]
   }
 };
