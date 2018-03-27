0f41d31304b8fc4406b5405a486926b41f6806ac
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-03-27 17:32:34 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,13 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+var App = require("./components/App.js");
 require("./index.css");
-
-class App extends React.Component {
-	render() {
-		return (
-			<div>React</div>
-		);
-	}
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
