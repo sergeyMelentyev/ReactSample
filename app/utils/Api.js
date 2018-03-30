04b428a46c0a7e0fd625d9051175e62d7458562b
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-03-30 16:21:54 +0300

    Create Api.js

---
+++ app/utils/Api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default {
+	fetchPopularRepos: function(language) {
+		var encodedURI = window.encodeURI("https://api.github.com/search/repositories?q=stars:>1+language:"+language+"&sort=stars&order=desc&type=Repositories");
+		return axios.get(encodedURI).then(response => response.data.items);
+	}
+};
