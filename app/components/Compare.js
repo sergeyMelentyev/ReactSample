e00af18ea7275b16bdc73a161accfaa7c6fe81e1
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-03-30 16:25:51 +0300

    Create Compare.js

---
+++ app/components/Compare.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				The most popular GitHub repositories.
+				Please visit "Popular" link in order to get information.
+			</div>
+		);
+	}
+}
+
+export default Home;
