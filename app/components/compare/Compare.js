2df87cc2dc98e1718a5e92dcbde239f1d3de3c7b
Sergey Melentyev <sergey.melentyev@gmail.com>
2018-04-02 13:19:10 +0300

    new version

--- app/components/compare/Compare.js
+++ app/components/compare/Compare.js
@@ -5,22 +5,22 @@ class Compare extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			firstRepoName: undefined,
-			firstRepoImage: undefined,
-			secondRepoName: undefined,
-			secondRepoImage: undefined
+			firstRepoName: "",
+			firstRepoImage: "",
+			secondRepoName: null,
+			secondRepoImage: null
 		};
-		this.onSubmit = this.onSubmit.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	onSubmit(id, repoName) {
+	handleSubmit(id, repoName) {
 		this.setState(() => {
 			var newState = new Object();
 			Object.defineProperties(newState, {
-	        	[id + "Name"]: {value: repoName},
-	        	[id + "Image"]: {value: `https://github.com/${repoName}.png?size=200`}
+	        	[id + "Name"]: {value: repoName, writable: true, enumerable: true, configurable: true},
+	        	[id + "Image"]: {value: `https://github.com/${repoName}.png?size=200`, writable: true, enumerable: true, configurable: true}
 			});
 			return newState;
-    	});
+		});
 	}
 	render() {
 		var firstRepoName = this.state.firstRepoName;
@@ -28,9 +28,9 @@ class Compare extends React.Component {
 		return (
 			<div className="repoInputContainer">
 				{!firstRepoName && 
-					<RepoInput id={firstRepoName} label="First Repository" onSubmit={this.onSubmit} />}
+					<RepoInput id="firstRepo" label="First Repository" onSubmit={this.handleSubmit} />}
 				{!secondRepoName &&
-					<RepoInput id={secondRepoName} label="Second Repository" onSubmit={this.onSubmit} />}
+					<RepoInput id="secondRepo" label="Second Repository" onSubmit={this.handleSubmit} />}
 			</div>
 		);
 	}
--- app/components/compare/RepoInput.js
+++ app/components/compare/RepoInput.js
@@ -1,8 +1,30 @@
 import React from "react";
-export default function RepoInput(props) {
-	return (
-		<div className="repoInput">
-			
-		</div>
-	);
+
+export default class RepoInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: ""
+		};
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+    	this.setState({name: event.target.value});
+  	}
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.onSubmit(this.props.id, this.state.name);
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit} className="repoInput">
+				<label>{this.props.label}
+					<input value={this.state.name} onChange={this.handleChange}
+						   type="text" placeholder="github username" autoComplete="off" />
+				</label>
+				<input type="submit" value="Submit" disabled={!this.state.name} />
+			</form>
+		);
+	}
 };
